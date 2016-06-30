e7dc7f4a34ef8f7178093c3951375a75134aa5a8
David Hershey <David Hershey>
2016-06-29 22:47:09 -0700

    Adding blank config file

---
+++ tf_seq2seq_chatbot/lib/cfg.py
@@ -0,0 +1,6 @@
+# cfg.py
+HOST = "irc.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = ""            # your Twitch username, lowercase
+PASS = "" # your Twitch OAuth token
+CHAN = ""                   # the channel you want to join
