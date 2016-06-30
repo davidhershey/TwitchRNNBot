1f8e92cda881e279c6865e051773bf02fe9de3c4
David Hershey <David Hershey>
2016-06-29 22:49:47 -0700

    Updated config file

--- tf_seq2seq_chatbot/lib/cfg.py
+++ tf_seq2seq_chatbot/lib/cfg.py
@@ -2,5 +2,9 @@
 HOST = "irc.twitch.tv"              # the Twitch IRC server
 PORT = 6667                         # always use port 6667!
 NICK = ""            # your Twitch username, lowercase
-PASS = "" # your Twitch OAuth token
-CHAN = ""                   # the channel you want to join
+PASS = "" # your Twitch OAuth token, eg oauth:xxxxxxxxxxxxxxxxxxxxxxxxx
+CHAN = ""                   # the channel you want to join, eg #nl_kripp
+
+if NICK == "" or PASS == "" or CHAN == "":
+    print "Please add account and channel info to tf_seq2seq_chatbot/lib/cfg.py"
+    exit(-1)
