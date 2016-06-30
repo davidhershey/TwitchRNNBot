0c72ae54380abb9dd465e6cd6a8278bec1857eef
David Hershey <dmakian@gmail.com>
2016-06-29 22:42:31 -0700

    Updates

--- tf_seq2seq_chatbot/lib/cfg.py
+++
@@ -1,6 +0,0 @@
-# cfg.py
-HOST = "irc.twitch.tv"              # the Twitch IRC server
-PORT = 6667                         # always use port 6667!
-NICK = "kripprnnbot"            # your Twitch username, lowercase
-PASS = "oauth:s4sunzcp7n5u1phk7g8kel5gwi6mit" # your Twitch OAuth token
-CHAN = "#hsdogdog"                   # the channel you want to join
